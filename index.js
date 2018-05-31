e30accc63a8aeff5d4d61d06727c210828232969
rjmcbain <rmcbain01@gmail.com>
2018-05-31 15:42:55 -0600

    adding components

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ import React from "react";
 import ReactNative from "react-native";
 
 // Create a component
+const App = () => {
+  return <Text>Some Text</Text>;
+};
 
 // Render it to the device
+ReactNative.AppRegistry.registerComponent("albums", () => App);
