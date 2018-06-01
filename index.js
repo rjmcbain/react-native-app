82b4b80ba4532e85ecc92ccb2015eeed238de507
rjmcbain <rmcbain01@gmail.com>
2018-06-01 15:08:30 -0600

    adding header component

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Import a library to help create a component
 import React from "react";
-import { Text, AppRegistry } from "react-native";
+import { AppRegistry } from "react-native";
 import Header from "./src/components/header";
 
 // Create a component
 const App = () => {
-  <Header />;
+  return <Header headerText={"Spinnr."} />;
 };
 
 // Render it to the device
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,35 @@
 // Import libraries for making a component
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 // Make a component
-const Header = () => {
-  return <Text>Albums!</Text>;
+const Header = props => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: "#EA4049",
+    alignItems: "center",
+    height: 70,
+    paddingTop: 15,
+    shadowColor: "#222",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    elevation: 2,
+    position: "relative"
+  },
+  textStyle: {
+    marginBottom: 10,
+    fontSize: 40,
+    color: "#eedec4"
+  }
 };
 
 // Make the component available to toher parts of the app
