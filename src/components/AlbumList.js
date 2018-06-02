139d0eda7ab62e56112ba23fd4ecd366cd34cb39
rjmcbain <rmcbain01@gmail.com>
2018-06-02 16:18:50 -0600

    adding class based components

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import axios from "axios";
 
 class AlbumList extends Component {
+  state = { albums: [] };
   componentWillMount() {
-    console.log("componentWillMount in AlbumList");
+    axios
+      .get("https://rallycoding.herokuapp.com/api/music_albums")
+      .then(response => this.setState({ albums: response.data }));
   }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
   render() {
-    return (
-      <View>
-        <Text>Album List!!</Text>
-      </View>
-    );
+    console.log(this.state);
+    return <View>{this.renderAlbums()}</View>;
   }
 }
 
